2cc14d4842992e1e4772968701e38b4d7d33f06c
Marc J. Schmidt <marc@marcjschmidt.de>
2019-03-17 01:48:16 +0100

    moved to nginx deployment

--- website/server.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static('../docs'));
-
-app.listen(port, () => console.log(`App listening on port ${port}!`));
